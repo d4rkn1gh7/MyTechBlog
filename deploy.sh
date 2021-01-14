912abe46da76b9b602084de763a2862bf4a57293
d4rkn1gh7 <balaji006@protonmail.com>
2021-01-14 13:25:43 +0530

    "site updated and added chillhack writeup"

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,38 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+printf "\033[0;32mPushing source code to MyBlog...\033[0m\n"
 
+echo "deleting public directory"
+rm -rf public
+
+echo "generating public"
+
+hugo -v
+
+echo "copying public to blog site"
+cp -rfv public/* ~/d4rkn1gh7.github.io
+
+echo "removed public"
+rm -rf public
+
+git add .
+
+echo "Enter commit message"
+read msg
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+git commit -m "$msg" # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+git push origin main
 
 # Go To Public folder
-cd public
+cd ~/d4rkn1gh7.github.io 
 
+printf "\033[0;32mUpdating the website...\033[0m\n"
 # Add changes to git.
 git add .
 
 # Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
 git commit -m "$msg"
 
 # Push source and build repos.
